2d6a6e58bd083c384a5c9f1459e2abc2e497257d
Cameron Jenkinson <camjenkinson@gmail.com>
2018-02-09 09:17:15 +0100

    Add function descriptions to controllers

--- server/controllers/classifyController.js
+++ server/controllers/classifyController.js
@@ -2,30 +2,49 @@
 
 const WatsonClient = require('../services/watson');
 
-async function getClassifiers (ctx)  {
-
+/**
+ * List all classifications created
+ *
+ * @param {object} ctx The context object
+ */
+async function getClassifiers(ctx) {
   try {
-     
     const classifiers = await WatsonClient.listClassifiers();
     ctx.ok(classifiers);
 
   } catch (err) {
     console.log(err);
   }
-
 }
 
-async function classifyImage (ctx) {
-
+/**
+ * Classify an image through a collection classifier e.g es_public
+ *
+ * @param {object} ctx The context object
+ */
+async function classifyImage(ctx) {
   try {
+    const data = await WatsonClient.classify();
+    // if threshold is below 2 then throw back unreliable match response
 
-    const label = await WatsonClient.classify();
-    ctx.ok(label);
+    // TODO: Manage endpoints for no class returned, prompts user to search or show related
+    // if (!data.images[0].classifiers[0].classes) {
+    //   throw new Error('No class returned');
+    // }
+
+    ctx.ok(data);
+
+    // const filterByThreshold = (label) => label.score > 0.2;
+    // const landmarkID = data.images[0].classifiers[0].classes.filter(filterByThreshold)[0].class
+    // TODO: Pass landmarkID to match with NLP service
 
   } catch (err) {
     console.log(err);
+    ctx.send({ 'error': 'No class returned' });
   }
-  
 }
 
-module.exports = { getClassifiers, classifyImage };
+module.exports = {
+  getClassifiers,
+  classifyImage
+};
--- server/controllers/conversationController.js
+++ server/controllers/conversationController.js
@@ -3,9 +3,15 @@
 const fs = require('fs');
 const WitClient = require('../services/wit');
 
-const context = {};
+/**
+ * Returns an array of responses based on the intent to reply with
+ *
+ * @param {string} intent The intent used to match a reply
+ */
+async function getReply (intent) {
 
-const getResponse = (intent) => {
+  // todo: refactor to switch statement
+  
   const responses = [];
 
   if (intent === 'hisona') {
@@ -16,31 +22,61 @@ const getResponse = (intent) => {
     responses.push('I am the Hisona AI, able to direct you to Hisonified landmarks and artefacts around the world.');
   }
   
-  if (context.defaultIntent) {
+  if (!intent) {
     responses.push('Default: there is no response for this intent');
   }
 
   return responses;
 }
 
-async function replyWithMessage (ctx) {
-
+/**
+ * Returns a list of intents based on the message received
+ *
+ * @param {object} data The incoming message 
+ */
+async function getIntent (data) {
   try {
-    const { body } = ctx.request;
-    const intentData = await WitClient.message(body.msgStr);
+    const landmarkID = data.landmarkID;
+    const msgToUnderstand = data.msgStr;
 
-    if (!intentData.entities.intent) {
-      throw new Error(`No intents match "${body.msgStr}"`);
+    const witClient = await WitClient.createInstance(landmarkID);
+    const intentData = await witClient.message(msgToUnderstand);
+
+    if (!intentData) {
+      throw new Error(`No response from the client`);
     }
 
     const intent = intentData.entities.intent[0].value;
-    const response = await getResponse(intent);
 
-    const reply = {
-      intent,
-      replyStr: response
+    return intent;
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/**
+ * Send a reply message back based on the matching intent
+ *
+ * @param {object} ctx The context object
+ */
+async function replyWithMessage (ctx) {
+  const { body } = ctx.request;
+
+  try {
+    const intent = await getIntent(body);
+    const replyMsg = await getReply(intent);
+
+    if (!intent) {
+      throw new Error(`No intent match returning default`);
     }
 
+    const reply = {
+      landmarkID: body.landmarkID,
+      intent: intent,
+      reply: replyMsg
+    }
+    
     ctx.ok(reply);
 
   } catch (err) {
