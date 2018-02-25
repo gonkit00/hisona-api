4ab8473667d7a17903ffd2fbe8fc53cc63e0058a
Cameron Jenkinson <camjenkinson@gmail.com>
2018-02-25 17:07:44 +0000

    Remove access token handlers in Wit client

--- server/services/wit.js
+++ server/services/wit.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const { Wit, log } = require('node-wit');
 
 const WitClient = {
-  
 	async processMessage(artefactId, messageToUnderstand) {
 		const witClientInstance = await WitClient.createInstance(artefactId);
 		const intentData = await witClientInstance.message(messageToUnderstand);
@@ -14,21 +13,10 @@ const WitClient = {
 	/**
 	 * Returns the correct WIT app token for the artefact in context
 	 *
-	 * @param {string} artefact_id The ID of the artefact in context
+	 * @param {string} artefactId The ID of the artefact in context
 	 */
 	async createInstance(artefactId) {
-
-    let access_token;
-    
-		// read es_public.json and find key from classID then create instance with nlpservices token
-		if (artefactId === '1_es_pub_ramonberengueriii') {
-			access_token = process.env.RAMON_TOKEN;
-		} else if (artefactId === '2_es_pub_christophercolubus') {
-			access_token = process.env.COLUMBUS_TOKEN;
-		} else {
-			// default fallback, todo
-			access_token = process.env.HISONA_ACCESS_TOKEN;
-		}
+		let access_token = process.env.COLUMBUS_TOKEN;
 
 		try {
 			const instance = await new Wit({
@@ -43,5 +31,3 @@ const WitClient = {
 };
 
 module.exports = WitClient;
-
-
