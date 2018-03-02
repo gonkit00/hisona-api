0975a37b0deb823c074bd29916b1df6f92c7b7fd
Cameron Jenkinson <camjenkinson@gmail.com>
2018-03-02 12:16:23 +0100

    Chore: move unused services to archive

--- server/services/index.js
+++ server/services/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const WatsonClient = require('../services/watson');
-const WitClient = require('../services/wit');
 const ConversationLogic = require('../services/conversation-logic');
 
 module.exports = {
   WatsonClient,
-  WitClient,
   ConversationLogic
 }
