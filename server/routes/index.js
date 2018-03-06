1a08eb1ea577ac6f82bef655aaf1573fe3983851
Cameron Jenkinson <camjenkinson@gmail.com>
2018-03-06 15:05:47 +0100

    Update index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ const router = new Router({
 const classify = require('../routes/classify.routes');
 const user = require('../routes/user.routes');
 
-router.get('/', async ctx => ctx.ok('Hisona API V1.0.0 online'));
+router.get('/', async ctx => ctx.ok('Hisona API Status: v1.0.0 available'));
 
 router.use('/classification', classify.routes());
 router.use('/user', user.routes());
